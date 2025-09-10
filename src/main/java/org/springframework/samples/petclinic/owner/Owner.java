8bab9f7957db63c491e68270526aa823d06aced8
Your Name <you@example.com>
2025-09-10 10:34:38 +0200

    Change again

--- src/main/java/org/springframework/samples/petclinic/owner/Owner.java
+++ src/main/java/org/springframework/samples/petclinic/owner/Owner.java
@@ -155,15 +155,11 @@ public class Owner extends Person {
 	@Override
 	public String toString() {
 		return new ToStringCreator(this)
-				.append("id", this.getId())
-				.append("new", this.isNew())
-				.append("lastName", this.getLastName())
-				.append("firstName", this.getFirstName())
-				.append("address", this.address)
-				.append("city", this.city)
-				.append("telephone", "DEMO-" + this.telephone) // ← MODIFICATION ICI
-				.append("email", "demo-change@launchable.com")   // ← NOUVEAU CHAMP
-				.toString();
+
+				.append("id", this.getId()).append("new", this.isNew()).append("lastName", this.getLastName())
+				.append("firstName", this.getFirstName()).append("address", this.address).append("city", this.city)
+				.append("telephone", this.telephone).toString();
+				
 	}
 
 	
