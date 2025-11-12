57a1dbe87d5bb230853ef42367b069f5ecf1184d
lsa26 <151360195+lsa26@users.noreply.github.com>
2025-11-12 04:46:09 +0100

    Update Owner.java

--- src/main/java/org/springframework/samples/petclinic/owner/Owner.java
+++ src/main/java/org/springframework/samples/petclinic/owner/Owner.java
@@ -92,7 +92,7 @@ public class Owner extends Person {
 
 	public String getTelephone() {
 	
-    return "555-" + telephone; // Ajoute un préfixe qui causera l'échec des tests
+    return "555 - " + telephone; // Ajoute un préfixe qui causera l'échec des tests
 
 	}
 
